225bea139cbdb78ce8dc857f7175a5ca6a3b4bf5
Ajit Patil <ajitpatil30@gmail.com>
2025-04-01 18:42:05 -0400

    updated title and included images

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -10,30 +10,30 @@ import { ProductCardComponent } from './product-card/product-card.component';
 })
 export class ProductListComponent {
 
-  async ngOnInit(){
-    const res = await fetch('https://fakestoreapi.com/products');
-    const data = await res.json();
-    this.products.set(data);
-  }
+  // async ngOnInit(){
+  //   const res = await fetch('https://fakestoreapi.com/products');
+  //   const data = await res.json();
+  //   this.products.set(data);
+  // }
   products = signal<Product[]>([
-    // {
-    //   id: 1, title: 'Product 1', price: 100,
-    //   image: 'https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg',
-    //   stock: 5
-    // },
-    // {
-    //   id: 2, title: 'Product 2', price: 200,
-    //   image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    {
+      id: 1, title: 'Opna Women\'s Short', price: 100,
+      image: 'https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg',
+      stock: 5
+    },
+    {
+      id: 2, title: 'Backpack, Fits 15 Laptops', price: 200,
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
       
-    // },
-    // {
-    //   id: 3, title: 'Product 3', price: 300,
-    //   image: 'https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg',
-    //   stock: 3
-    // },{
-    //   id: 4, title: 'Product 4', price: 400,  
-    //   image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
-    //   stock: 2
-    // }
+    },
+    {
+      id: 3, title: 'Rain Jacket Women', price: 300,
+      image: 'https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg',
+      stock: 3
+    },{
+      id: 4, title: 'Mens Cotton Jacket', price: 400,  
+      image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+      stock: 2
+    }
   ]);
 }
